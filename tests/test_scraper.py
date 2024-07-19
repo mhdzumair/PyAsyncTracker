03a8b30ddd6b97a96cc06d383078fc56b80e7e60
mhdzumair <mhdzumair@gmail.com>
2024-07-19 21:44:32 +0530

    release v0.3.0

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -4,8 +4,8 @@ from pyasynctracker import scrape_info_hashes, batch_scrape_info_hashes
 
 @pytest.mark.asyncio
 async def test_scrape_info_hashes():
-    info_hashes = ["2b66980093bc11806fab50cb3cb41835b95a0362"]
-    trackers = ["http://bttracker.debian.org:6969/announce"]
+    info_hashes = ['bceb15ae55e17ae765af504a8f645595b936aefa']
+    trackers = ["udp://tracker.opentrackr.org:1337/announce", "https://tracker1.ctix.cn:443/announce", "udp://open.demonii.com:1337/announce", "https://tracker.loligirl.cn:443/announce", "udp://open.stealth.si:80/announce", "udp://tracker.torrent.eu.org:451/announce", "udp://tracker-udp.gbitt.info:80/announce", "udp://exodus.desync.com:6969/announce", "https://tracker.gbitt.info:443/announce", "http://tracker.gbitt.info:80/announce", "http://bvarf.tracker.sh:2086/announce", "udp://tracker.tryhackx.org:6969/announce", "udp://tracker.tiny-vps.com:6969/announce", "udp://tracker.picotorrent.one:6969/announce", "udp://tracker.0x7c0.com:6969/announce"]
 
     results = await scrape_info_hashes(info_hashes, trackers)
     assert isinstance(results, dict)
